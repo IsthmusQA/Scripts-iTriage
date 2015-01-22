c770b963c184c54b7475cd1bcc0113e5806b589f
Nelson Alfaro <nelsonalfaro@Nelsons-MacBook-Air.local>
2015-01-22 12:00:15 -0600

    testing
    testing

--- Setup/setup.rb
+++ Setup/setup.rb
@@ -1,6 +1,7 @@
 #this is the test-suit, we can decide which scripts want to run or if we want to run them all
 
+File.expand_path("../documents/github/scripts-itriage/main_itriage.rb", __FILE__)
 #require_relative 'medications-list'
 #require_relative 'procedures-nav'
-require_relative 'my_itriageInsurance' 
-require_relative 'passwordrequired' 
+#require_relative 'my_itriageInsurance' 
+#require_relative 'passwordrequired' 
